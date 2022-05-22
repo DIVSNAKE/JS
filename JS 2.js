2ef2da2fcb6237a4dad8d6324bb8ed40dae658a1
DIVSNAKE <DIV.SNAKE@yandex.by>
2022-05-22 13:17:23 +0300

    add new files HW JS

---
+++ JS 2.js
@@ -0,0 +1,24 @@
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+
+// console.log(true && true);
+// console.log(true && false);
+// // console.log(false && true);
+
+// let item_1 = 5;
+// let item_2 = 100;
+// let item_3 = 30;
+
+
+
+// if (item_1 > 3 || item_2 < 15 && item_3 ==50){
+//     console.log("Idi Kuhat")
+// } else {
+//     console.log("Hodi golodniy")
+// }
+
+
+while(true){
+    console.log(item_1 + "Idi Kuhat")
+}
